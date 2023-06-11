e42f6466b1ba11773bef5db9a145627d1d68f3df
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2023-06-11 17:34:08 +0200

    19.3 Delete task

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,12 +1,11 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +16,16 @@ public class DbService {
     public List<Task> getAllTasks() {
         return repository.findAll();
     }
-    public Optional<Task> getTask(Long taskId){
-            return repository.findById(taskId);
+
+    public Task saveTask(Task task) {
+        return repository.save(task);
+    }
+
+    public Task getTask(Long id) throws TaskNotFoundException {
+            return repository.findById(id).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public void deleteTask(Task task) {
+        repository.delete(task);
     }
 }
