b0af3fee6014fec746191b6ce29f93346d938a1c
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2023-08-18 13:02:40 +0200

    Added Swagger

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -8,22 +8,24 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @OpenAPIDefinition
 @SpringBootApplication
-public class TasksApplication {
+public class TasksApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TasksApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
 	}
 }
 
-
-//public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication {
 //
 //	public static void main(String[] args) {
-//
 //		SpringApplication.run(TasksApplication.class, args);
 //	}
-//	@Override
-//	protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
-//		return application.sources(TasksApplication.class);
-//	}
 //}
+
+
+
