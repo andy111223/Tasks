cf37014fe1095164d5f07e68b6c9b3e85e131bf5
Andrzej Rzeszutek <andrzej.rzeszutek@gmail.com>
2023-09-30 23:14:36 +0200

    23.3 CC email added

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,22 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.Optional;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private final List<String> toCc;
+
+    public Optional <List<String>> getToCc() {
+        return Optional.ofNullable(toCc);
+    }
+}
